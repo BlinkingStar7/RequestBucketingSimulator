6503312506594a942ed9a5f3b5ac1a529192fa57
BlinkingStar7 <sean.hong@buzzvil.com>
2024-03-13 22:49:05 +0900

    Divide files into main

--- simulator.py
+++ simulator.py
@@ -72,28 +72,3 @@ class Simulator:
         print('-' * 50)
         print('Total\t' + f'{sum(self.impressions)}\t{sum(self.clicks)}\t{sum(len(self.unique_impressions[i]) for i in range(SimulatorConstants.NUMBER_OF_GROUPS.value))}\t\t{sum(len(self.unique_clicks[i]) for i in range(SimulatorConstants.NUMBER_OF_GROUPS.value))}')
         print('\n')
-
-if __name__ == "__main__":
-    user_bucketing_simulator = Simulator()
-    request_bucketing_simulator = Simulator()
-
-    while not user_bucketing_simulator.is_over():
-        id = user_bucketing_simulator.get_next_request()
-
-        # user bucketing only allocate user to group based on their id
-        if user_bucketing_simulator.can_allocate(id, id//SimulatorConstants.NUMBER_OF_PEOPLE_IN_GROUP.value):
-            user_bucketing_simulator.allocate(id, id//SimulatorConstants.NUMBER_OF_PEOPLE_IN_GROUP.value)
-
-    while not request_bucketing_simulator.is_over():
-        id = request_bucketing_simulator.get_next_request()
-
-        # request bucketing allocate user to group based on random number
-        random_group = random.randint(0, SimulatorConstants.NUMBER_OF_GROUPS.value - 1)
-        if request_bucketing_simulator.can_allocate(id, random_group):
-            request_bucketing_simulator.allocate(id, random_group)
-
-    # print a table of results
-    print('User Bucketing')
-    user_bucketing_simulator.print_results()
-    print('Request Bucketing')
-    request_bucketing_simulator.print_results()
